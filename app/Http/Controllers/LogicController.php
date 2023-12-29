fb4d95f129e6582eea582fe1e5a64dbe90ca14a7
aryo100 <aryo100@gmail.com>
2023-12-29 11:21:35 +0700

    fix same word func in logic controller

--- app/Http/Controllers/LogicController.php
+++ app/Http/Controllers/LogicController.php
@@ -8,33 +8,24 @@ class LogicController extends Controller
 {
     public function sameWord(Request $request)
     {
-        // Mendapatkan dua kalimat dari request
         $word1 = strtolower($request->word1);
         $word2 = strtolower($request->word2);
 
-        // Memeriksa panjang kedua kalimat harus sama
         if (strlen($word1) !== strlen($word2)) {
             return response()->json(['message' => 'Panjang kedua kalimat harus sama'], 400);
         }
 
-        // Memeriksa setiap huruf pada kedua kalimat
-        $char1 = str_split($word1);
-        $char2 = str_split($word2);
-        $cond = true;
-        for ($i = 0; $i < strlen($char1); $i++) {
-            $charcond = false;
-            for ($j = 0; $j < strlen($char2); $j++) {
-                if ($char1[$i] === $char2[$j]) {
-                    $charcond = true;
-                }
-            }
-            $cond = $charcond;
-        }
-        if($cond){
+        $sortedWord1 = str_split($word1);
+        $sortedWord2 = str_split($word2);
+
+        sort($sortedWord1);
+        sort($sortedWord2);
+
+        $result = ($sortedWord1 == $sortedWord2);
+        if($result){
+            return response()->json(['message' => 'Semua huruf sama'], 200);
+        }else{
             return response()->json(['message' => 'Tidak semua huruf sama'], 400);
         }
-
-        // Jika semua huruf sama
-        return response()->json(['message' => 'Semua huruf sama'], 200);
     }
 }
